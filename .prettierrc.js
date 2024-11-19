7cb46a080f5ebf16d28549bf2112e493729f0047
Ramon <ramon@cordada.io>
2024-11-19 11:32:21 +0100

    wip

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 const config = {
-  cssDeclarationSorterOrder: "alphabetical",
-  plugins: [require.resolve("prettier-plugin-css-order")],
+  cssDeclarationSorterOrder: 'alphabetical',
+  plugins: [require.resolve('prettier-plugin-css-order')],
 };
 
 module.exports = config;
